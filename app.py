ccdf3f5f596dc9334c0395803d8dc2b16d083456
yasir-z <ysairch@gmail.com>
2026-02-02 07:53:25 +0000

    code quality test

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "DevOps Pipeline is Active!"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- app_test.py
+++ app_test.py
@@ -1,13 +1,15 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
     """Verify the home page loads correctly."""
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
     assert b"DevOps Pipeline is Active!" in response.data
